d43d987159de46552a0344c3af8028480869b14d
chandsgowda <chandansuresh007@gmail.com>
2022-05-19 20:13:49 +0530

    added todo object

---
+++ lib/providers/todo_object.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'todo_object.g.dart';
+
+@HiveType(typeId: 0)
+class TodoObject extends ChangeNotifier{
+
+  @HiveField(0)
+  final String title;
+
+  @HiveField(1)
+  final String description;
+
+  TodoObject(this.title, this.description);
+}
